3a795ee44cde6b9362e7801e1fa365d82d53f6e3
Ivan Novak <vananovak@ukr.net>
2023-01-04 20:58:32 +0200

    fix flash on login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
 
+  validates :first_name, :last_name, presence: true
+
+
   def will_save_change_to_email?
     false
   end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -309,4 +309,28 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   config.sign_out_via = :get
+  # frozen_string_literal: true
+
+  class TurboFailureApp < Devise::FailureApp
+    def respond
+      if request_format == :turbo_stream
+        redirect
+      else
+        super
+      end
+    end
+
+    def skip_format?
+      %w(html turbo_stream */*).include? request_format.to_s
+    end
+  end
+
+
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
+
+  config.warden do |manager|
+    manager.failure_app = TurboFailureApp
+    #   manager.intercept_401 = false
+    #   manager.default_strategies(scope: :user).unshift :some_external_strategy
+  end
 end
