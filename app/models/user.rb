ed524c18e329bdd8c53fac70d0f25e100f825ba2
Ivan Novak <vananovak@ukr.net>
2023-01-04 20:21:27 +0200

    add auth and fix some bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable
+
+  def will_save_change_to_email?
+    false
+  end
 end
